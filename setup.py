bb67432ebe8b8a2adcb1adaf62f5f514f2dc8955
Peter Tripp <peter.tripp@gmail.com>
2019-06-20 11:31:14 -0400

    axidraw-api_v230_b1

---
+++ setup.py
@@ -0,0 +1,36 @@
+"""
+Based on https://github.com/pypa/sampleproject
+
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+from os import path
+from io import open
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.txt'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='pyaxidraw',
+    version='2.3.0',
+    long_description=long_description,
+    long_description_content_type='text/plain',
+    url='https://axidraw.com/doc/cli_api/',
+    author='Evil Mad Scientist Laboratories',
+    author_email='contact@evilmadscientist.com',
+
+
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    install_requires=[
+        'lxml',
+        'pyserial>=2.7.0' # 3.0 recommended
+    ],
+    extras_require={
+        'dev': [],
+        'test': [],
+    },
+)
