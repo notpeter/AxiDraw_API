f952a3b653f0aae8e713b64a0afb21ce097b709f
Peter Tripp <peter@clamps.hsd1.ma.comcast.net>
2021-01-16 23:18:27 -0500

    ad_api_263_r1

--- axicli/__main__.py
+++ axicli/__main__.py
@@ -1,4 +1,13 @@
-from .core import axidraw_CLI
+from .axidraw_cli import axidraw_CLI
+
+try:
+    from .hta_cli import hta_CLI
+except ImportError as ie:
+    if "hta" in str(ie) or "hershey" in str(ie):
+        # this is probably ok, because it just means hershey advanced is not available on this installation
+        pass
+    else:
+        raise ie
 
 def main():
     axidraw_CLI()
