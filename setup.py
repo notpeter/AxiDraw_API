95ee77140cf8a515bd3aa425a72edab3b8968490
Peter Tripp <petertripp@gmail.com>
2021-03-26 12:35:40 -0400

    ad_api_263_r2

--- setup.py
+++ setup.py
@@ -5,6 +5,9 @@ Based on https://github.com/pypa/sampleproject
 
 # Always prefer setuptools over distutils
 import glob
+from io import open
+from os import path
+import re
 import setuptools
 import subprocess
 import sys
@@ -32,10 +35,21 @@ with open(path.join(here, 'README.txt'), encoding='utf-8') as f:
     long_description = f.read()
 
 def replacement_setup(*args, **kwargs):
-    depdir = "prebuilt_dependencies"
-    if path.isdir(depdir): #installing on a non-privileged machine; todo consider adding a check for the pip -e flag
-        for wheel in glob.glob("/".join([depdir, "*"])):
-            subprocess.check_call([sys.executable, '-m', 'pip', 'install', wheel])
+    try:
+        depdir = "prebuilt_dependencies"
+        if path.isdir(depdir): #installing on a non-privileged machine
+            pkg_pattern = re.compile('(?P<pkg>[a-zA-Z]*)-[0-9]')
+            for wheel_file in glob.glob(path.join(depdir, "*")):
+                pkg_name = pkg_pattern.search(wheel_file).group('pkg')
+                try:
+                    subprocess.check_call(
+                        [sys.executable, '-m', 'pip', 'uninstall', '--yes', pkg_name])
+                except subprocess.CalledProcessError: # Will be raised if there is no version to uninstall
+                    pass
+                subprocess.check_call([sys.executable, '-m', 'pip', 'install', wheel_file])
+    except (AttributeError, subprocess.CalledProcessError) as err:
+        raise RuntimeError("Could not install one or more prebuilt dependencies.")
+
     original_setup(*args, **kwargs)
 
 original_setup = setuptools.setup
